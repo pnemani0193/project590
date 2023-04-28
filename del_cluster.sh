622e24f5ecb8fd197e9299eed7bdcc9dde4d4ddd
pnemani0193 <pardhasaradhisastry.nemani@valpo.edu>
2023-04-28 14:37:50 +0000

    deletion

--- del_cluster.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-
-gcloud container clusters delete apc-pn --project=pn-project-374416 --region=us-central1
--- del_ds.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-
-kubectl delete service flaskapp-service && kubectl delete deployment flaskapp
